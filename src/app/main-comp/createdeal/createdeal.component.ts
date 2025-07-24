ab541b78e5391a2d8cd726a9d61672dcdcca771b
MrudulaSpec <mruduls@mynusco.com>
2025-07-24 12:59:03 +0530

    storesOptions

--- src/app/main-comp/createdeal/createdeal.component.ts
+++ src/app/main-comp/createdeal/createdeal.component.ts
@@ -169,8 +169,10 @@ export class CreatedealComponent {
             productRow.QUANTITY = this.rowData[i].quantity;
             productRow.DISCOUNT_RATE= this.rowData[i].discount;
             productRow.MEASURE_CODE = 'pcs';
-            productRow.WAREHOUSE_ID = this.rowData[i].WAREHOUSE_ID;
-            rowsList.push(productRow);
+            productRow.WAREHOUSE_ID = this.createDealForm.get("storesOptions").value;
+            //alert(this.createDealForm.get("storesOptions").value);
+            //alert(productRow.WAREHOUSE_ID.toString());
+           rowsList.push(productRow);
           } else {
             this.onFlashOneCell(i, 'quantity');
           }
